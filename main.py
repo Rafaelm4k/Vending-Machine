afd0c48ef1f2b1b5cded7e6e5ac0bd2d79ee9e34
Rafaelm4k <rafaeldavidteran2019@gmail.com>
2025-01-30 00:22:10 +0100

    def ProductoElegido y Opcion de pago

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 """
 Maquina Expendedora
 
-1 def funciones 
-2 podemos crear arrays bidimensionales o dos arrays, productos y precios
-
-
 """
+import os 
+
 Productos = ["Agua 💧" , "Refresco 🥤" , "Zumo 🧃" ]
 Precios = [0.50,0.75,0.95]
 ReservaMonedas = [20,20,20,20,20]
@@ -20,4 +18,21 @@ def menu(nombres,precios):
     textomenu += f"{cont+1} - Salir 🚪"
     return textomenu
 
-print(menu(Productos,Precios))
+def ProductoElegido(opcion, Productos , Precios):
+    if opcion == 1 or opcion == 2 or opcion == 3 :
+        return f"\nHaz Escogido {Productos[opcion - 1]} \nTotal a Pagar : {Precios[opcion - 1]}"
+    elif opcion == 4:
+        return "¡PROGRAMA FINALIZADO!"
+
+continuar = True
+while continuar : 
+    print(menu(Productos,Precios))
+    opcion = int(input("Escoge una Opcion "))
+    os.system("cls")
+    if opcion == 4:
+        print(ProductoElegido(opcion,Productos,Precios))
+        continuar = False
+    else:
+        print(ProductoElegido(opcion,Productos,Precios))
+        pago = float(input("Ingresa Las monedas necesarias para pagar el producto "))
+    
